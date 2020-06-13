f2edb0d3aa2c864dff007b2cc5dd32dd5b99fc6e
shadowrider38 <connorwang38@gmail.com>
2020-06-13 16:56:27 -0500

    Added template for 7 levels and main page

--- Level 1/script.js
+++ Level 1/script.js
@@ -86,41 +86,46 @@ class Calculator {
   }
   
   
-  const numberButtons = document.querySelectorAll('[data-number]')
-  const operationButtons = document.querySelectorAll('[data-operation]')
-  const equalsButton = document.querySelector('[data-equals]')
-  const deleteButton = document.querySelector('[data-delete]')
-  const allClearButton = document.querySelector('[data-all-clear]')
-  const previousOperandTextElement = document.querySelector('[data-previous-operand]')
-  const currentOperandTextElement = document.querySelector('[data-current-operand]')
-  
-  const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
-  
-  numberButtons.forEach(button => {
+const numberButtons = document.querySelectorAll('[data-number]')
+const operationButtons = document.querySelectorAll('[data-operation]')
+const equalsButton = document.querySelector('[data-equals]')
+const deleteButton = document.querySelector('[data-delete]')
+const allClearButton = document.querySelector('[data-all-clear]')
+const previousOperandTextElement = document.querySelector('[data-previous-operand]')
+const currentOperandTextElement = document.querySelector('[data-current-operand]')
+const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
+
+document.addEventListener('click', () => {
+    if (calculator.currentOperand == 20) {
+        console.log("Good job!");
+    }
+})
+
+numberButtons.forEach(button => {
     button.addEventListener('click', () => {
-      calculator.appendNumber(button.innerText)
-      calculator.updateDisplay()
+        calculator.appendNumber(button.innerText)
+        calculator.updateDisplay()
     })
   })
   
-  operationButtons.forEach(button => {
+operationButtons.forEach(button => {
     button.addEventListener('click', () => {
       calculator.chooseOperation(button.innerText)
       calculator.updateDisplay()
     })
   })
   
-  equalsButton.addEventListener('click', button => {
+equalsButton.addEventListener('click', button => {
     calculator.compute()
     calculator.updateDisplay()
-  })
+})
   
-  allClearButton.addEventListener('click', button => {
+allClearButton.addEventListener('click', button => {
     calculator.clear()
     calculator.updateDisplay()
-  })
+})
   
-  deleteButton.addEventListener('click', button => {
+deleteButton.addEventListener('click', button => {
     calculator.delete()
     calculator.updateDisplay()
-  })
+})
